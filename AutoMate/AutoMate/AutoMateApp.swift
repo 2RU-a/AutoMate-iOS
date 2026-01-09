90892f7015bd4688416768100b80d4e64fda7d9c
2RU-a <orurua@hotmail.com>
2026-01-09 23:10:37 +0400

    chore: initialize AutoMate iOS project

---
+++ AutoMate/AutoMate/AutoMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMateApp.swift
+//  AutoMate
+//
+//  Created by oto rurua on 09.01.26.
+//
+
+import SwiftUI
+
+@main
+struct AutoMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
